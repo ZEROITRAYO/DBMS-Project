2fa7594c6aa778bcc114fa616b23e89d8e04d024
ZEROITRAYO <117174414+ZEROITRAYO@users.noreply.github.com>
2024-03-09 11:15:55 +0530

    updated

--- services/authentication.js
+++ services/authentication.js
@@ -1,10 +1,9 @@
 require('dotenv').config();
-const { response } = require('express');
 const jwt=require('jsonwebtoken');
 
 function authenticateToken(req,res,next){
     const authHeader=req.headers['authorization']
-    const token=authHeader && authHeader.split[' '][1]
+    const token=authHeader && authHeader.split(' ')[1]
     if(token==null){
         return res.sendStatus(401);
 
@@ -13,6 +12,11 @@ function authenticateToken(req,res,next){
     jwt.verify(token,process.env.ACCESS_TOKEN,(err,response)=>{
         if(err){
             return res.sendStatus(403);
+          
         }
+        res.locals=response;
+        next()
     })
 }
+
+module.exports={authenticateToken: authenticateToken}
